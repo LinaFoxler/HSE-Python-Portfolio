36fe2e3936356c5d10aa0a96e6d51b85524db901
Foxler <nat254512@rambler.ru>
2024-03-22 19:11:52 +0500

    Доработка

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     # созданные приложения
     "jobs.apps.JobsConfig",
     "blog.apps.BlogConfig",
-    # установленные приложения
+    "author.apps.AuthorConfig",
     "ckeditor",
     "ckeditor_uploader",
 ]
@@ -73,6 +73,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "author.context_processors.author",
             ],
         },
     },
